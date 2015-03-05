3941d38bf40396c984884acf1ffc10ce62efc10e
gmccurdy <gerald.mccurdy@stgconsulting.com>
2015-03-04 22:59:13 -0700

    Code Challenge 12
    Visual Studio 2013 MVC 5 project

---
+++ gmccurdy/CodeChallenge12/CodeChallenge12/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeChallenge12.Startup))]
+namespace CodeChallenge12
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
